24c18ca2b0883f0395070a2474efa890006041af
Martin <martincanon@users.noreply.github.com>
2018-04-12 09:14:13 -0500

    Update plot.desc.R

--- plot.desc.R
+++ plot.desc.R
@@ -1,8 +1,14 @@
-plot.desc <- function(x) {
+plot.desc <- function(x, main) {
   par(mfrow = c(2, 2))
-  plot(density(x, na.rm = TRUE))
-  hist(x)
-  qqnorm(x)
+  if(missing(main))
+    main <- deparse(substitute(x))
+  if(grepl("\\$", main)) {
+    main <- strsplit(main, "\\$")[[1]][2]
+  }
+  plot(x, main = main)
+  plot(density(x, na.rm = TRUE), main = main)
+  hist(x, main = main)
+  qqnorm(x, main = main)
   qqline(x, col = "green", lwd = 2, lty = 2)
   par(mfrow = c(1, 1))
 }
